1645078536bc7a3fd0b674c156fa32c3d4ada54e
zehfernandes <ozehfernandes@gmail.com>
2015-02-23 11:37:17 -0300

    Tweet button javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+var currentExpression;
+
 function requestWords() {
 	var request = new XMLHttpRequest();
 	request.open('GET', '/hello', true);
@@ -6,6 +8,7 @@ function requestWords() {
 	  if (request.status >= 200 && request.status < 400) {
 	    // Success!
 	    var resp = request.responseText;
+	    currentExpression = resp;
 		document.getElementById("expression").innerHTML = resp;
 
 	  } else {
@@ -23,7 +26,21 @@ function requestWords() {
 	return false;
 }
 
+function openTweetBox(evt) {
+
+	var width = 550;
+  	var height = 350;
+  	var left = window.innerWidth/2 - width/2;
+  	var top = window.innerHeight/2 - height/2;
+  	var url = 'http://twitter.com/share?url=http://gourmetstrike.herokuapp.com&text=' + currentExpression + '&hashtags=gourmetstrike&';
+
+  	window.open(url,'janela', 'width='+width+', height='+height+', top='+top+', left='+left+', scrollbars=no, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
+
+  	evt.preventDefault();
+
+}
 
 document.getElementById("retry").addEventListener("click", requestWords);
+document.getElementById("tweet").addEventListener("click", openTweetBox);
 
 //requestWords();
