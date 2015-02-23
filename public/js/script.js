c4b39d564106e28dba5543b1122a50e920ec9e60
zehfernandes <ozehfernandes@gmail.com>
2015-02-23 14:54:47 -0300

    About modal

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
+(function() {
+
 var currentExpression;
 var generate = true;
 
-function requestWords() {
+function requestWords(evt) {
 
 	if (generate == true) {
 		generate = false;
@@ -17,10 +19,10 @@ function requestWords() {
 
 		    $_expression = document.getElementById("expression");
 			$_expression.innerHTML = resp;
-			$_expression.className =  "pulse";
+			classie.add( $_expression, 'pulse' );
 
-			setTimeout(function(){ 
-				$_expression.className = "";
+			setTimeout(function(){
+				classie.remove( $_expression, 'pulse' );
 				generate = true;
 			}, 500);
 
@@ -64,9 +66,53 @@ function spaceBarShortCut(evt) {
 	evt.preventDefault();
 }
 
+//Listeners
 document.getElementById("retry").addEventListener("click", requestWords);
 document.getElementById("tweet").addEventListener("click", openTweetBox);
 document.addEventListener("keyup", spaceBarShortCut, false);
 
 
-//requestWords();
+
+//OPEN MODAL
+var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+		}
+	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+
+
+})();
