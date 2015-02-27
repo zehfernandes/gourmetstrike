8a134b2e76aef69bbc0d82159395cb27d1136c16
zehfernandes <ozehfernandes@gmail.com>
2015-02-27 04:30:23 -0300

    CSS fixing IPAD and Iphone

--- http.js
+++ http.js
@@ -121,6 +121,6 @@ router.get("/en", function(request, response) {
 }); */
 
 var server = http.createServer(router);
-server.listen(process.env.PORT || 80, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log('listening on', server.address().port);
 });
